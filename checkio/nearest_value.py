5a8c53833382745f3faffd86ffc16e02e0b4954a
kdvoyashov <konstantin.dvoyashov@comply.co.il>
2020-08-15 12:03:35 +0300

    neares value

---
+++ checkio/nearest_value.py
@@ -0,0 +1,36 @@
+def nearest_value(values: set, one: int) -> int:
+    res = 0
+    first = True
+    for val in sorted(values):
+        if first:
+            res = abs(one - val)
+            prev_val = val
+            first = False
+        else:
+            if res > abs(one - val):
+                res = abs(one - val)
+                prev_val = val
+            else:
+                return prev_val
+
+    '''
+    Best Solution
+    '''
+    return min(values, key=lambda n: (abs(one - n), n))
+
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(nearest_value({-1, 2, 3}, 0))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    # assert nearest_value({4, 7, 10, 11, 12, 17}, 9) == 10
+    # assert nearest_value({4, 7, 10, 11, 12, 17}, 8) == 7
+    # assert nearest_value({4, 8, 10, 11, 12, 17}, 9) == 8
+    # assert nearest_value({4, 9, 10, 11, 12, 17}, 9) == 9
+    # assert nearest_value({4, 7, 10, 11, 12, 17}, 0) == 4
+    # assert nearest_value({4, 7, 10, 11, 12, 17}, 100) == 17
+    # assert nearest_value({5, 10, 8, 12, 89, 100}, 7) == 8
+    # assert nearest_value({-1, 2, 3}, 0) == -1
+    # print("Coding complete? Click 'Check' to earn cool rewards!")
