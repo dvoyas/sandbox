b378d5159636c6948558badfdd190c91a0d6e699
kdvoyashov <konstantin.dvoyashov@comply.co.il>
2020-08-20 12:07:48 +0300

    is all upper 2

--- checkio/is_all_upper.py
+++ checkio/is_all_upper.py
@@ -1,3 +1,5 @@
+import re
+
 def is_all_upper(text: str) -> bool:
     all_not_letter = True
     for letter in text.replace(" ",""):
@@ -18,4 +20,20 @@ if __name__ == '__main__':
     assert is_all_upper('all lower') == False
     assert is_all_upper('mixed UPPER and lower') == False
     assert is_all_upper('') == True
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
+
+def is_all_upper(text: str) -> bool:
+    return text.upper() == text and text != '' and bool(re.search('[a-zA-Z]', text))
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_all_upper('ALL UPPER'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_all_upper('ALL UPPER') == True
+    assert is_all_upper('all lower') == False
+    assert is_all_upper('mixed UPPER and lower') == False
+    assert is_all_upper('') == False
     print("Coding complete? Click 'Check' to earn cool rewards!")
