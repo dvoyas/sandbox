15616868bd518b22b79ad35625a5a671a0d2ceb1
kdvoyashov <konstantin.dvoyashov@comply.co.il>
2020-08-23 22:37:32 +0300

    is ascending

---
+++ checkio/is_ascending.py
@@ -0,0 +1,34 @@
+from typing import Iterable
+
+
+def is_ascending(items: Iterable[int]) -> bool:
+    i = len(items)
+    if i == 0 or i == 1:
+        return True
+    i -= 1
+    res = False
+    while i > 0:
+        if items[i] > items[i-1]:
+            res = True
+            i -= 1
+        else:
+            return False
+
+    return res
+
+# OTHER SOLUTIONS
+#     return all(items[x]>items[x-1]for x in range(1,len(items)))
+
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_ascending([4, 5, 6, 7, 3, 7, 9]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_ascending([-5, 10, 99, 123456]) == True
+    assert is_ascending([99]) == True
+    assert is_ascending([4, 5, 6, 7, 3, 7, 9]) == False
+    assert is_ascending([]) == True
+    assert is_ascending([1, 1, 1, 1]) == False
+    print("Coding complete? Click 'Check' to earn cool rewards!")
