42acfe0ab373e4a4930cd06e9a3394d698b03938
kdvoyashov <konstantin.dvoyashov@comply.co.il>
2020-09-04 22:01:28 +0300

    sum by types

---
+++ checkio/sum_by_types.py
@@ -0,0 +1,26 @@
+from typing import Tuple
+
+def sum_by_types(items: list) -> Tuple[str, int]:
+    # your code here
+    s = ''
+    i = 0
+    for item in items:
+        if type(item) == str:
+            s += item
+        else:
+            i += item
+    return (s, i)
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(sum_by_types([]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert sum_by_types([]) == ('', 0)
+    assert sum_by_types([1, 2, 3]) == ('', 6)
+    assert sum_by_types(['1', 2, 3]) == ('1', 5)
+    assert sum_by_types(['1', '2', 3]) == ('12', 3)
+    assert sum_by_types(['1', '2', '3']) == ('123', 0)
+    assert sum_by_types(['size', 12, 'in', 45, 0]) == ('sizein', 57)
+    print("Coding complete? Click 'Check' to earn cool rewards!")
