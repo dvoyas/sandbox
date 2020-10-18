a2d21520e50d2d09b1291a763e425098d1178627
kdvoyashov <konstantin.dvoyashov@comply.co.il>
2020-10-18 08:09:34 +0300

    flask

---
+++ checkio/fight_army.py
@@ -0,0 +1,90 @@
+# Taken from mission The Warriors
+
+class Warrior:
+    def __init__(self, health=50, attack=5):
+        self.health = health
+        self.attack = attack
+
+    @property
+    def is_alive(self) -> bool:
+        return self.health > 0
+
+
+class Knight(Warrior):
+    def __init__(self, health=50, attack=7):
+        Warrior.__init__(self, health=50, attack=7)
+
+class Army:
+    def __init__(self):
+        add_units
+
+def fight(unit_1, unit_2):
+    while True:
+        unit_2.health -= unit_1.attack
+        if not unit_2.is_alive:
+            break
+        unit_1.health -= unit_2.attack
+        if not unit_1.is_alive:
+            break
+    return unit_1.is_alive
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+
+    chuck = Warrior()
+    bruce = Warrior()
+    carl = Knight()
+    dave = Warrior()
+    mark = Warrior()
+    carl.attack
+
+    assert fight(chuck, bruce) == True
+    assert fight(dave, carl) == False
+    assert chuck.is_alive == True
+    assert bruce.is_alive == False
+    assert carl.is_alive == True
+    assert dave.is_alive == False
+    assert fight(carl, mark) == False
+    assert carl.is_alive == False
+
+    print("Coding complete? Let's try tests!")
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+
+    # fight tests
+    chuck = Warrior()
+    bruce = Warrior()
+    carl = Knight()
+    dave = Warrior()
+    mark = Warrior()
+
+    assert fight(chuck, bruce) == True
+    assert fight(dave, carl) == False
+    assert chuck.is_alive == True
+    assert bruce.is_alive == False
+    assert carl.is_alive == True
+    assert dave.is_alive == False
+    assert fight(carl, mark) == False
+    assert carl.is_alive == False
+
+    # battle tests
+    my_army = Army()
+    my_army.add_units(Knight, 3)
+
+    enemy_army = Army()
+    enemy_army.add_units(Warrior, 3)
+
+    army_3 = Army()
+    army_3.add_units(Warrior, 20)
+    army_3.add_units(Knight, 5)
+
+    army_4 = Army()
+    army_4.add_units(Warrior, 30)
+
+    battle = Battle()
+
+    assert battle.fight(my_army, enemy_army) == True
+    assert battle.fight(army_3, army_4) == False
+    print("Coding complete? Let's try tests!")
